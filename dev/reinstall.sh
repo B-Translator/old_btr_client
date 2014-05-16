81ea3aa2a417d2d2d57c6f3f90b2d485169cbca1
Dashamir Hoxha <dashohoxha@gmail.com>
2014-05-16 18:26:28 +0200

    Fixed the reinstall script.

--- dev/reinstall.sh
+++ dev/reinstall.sh
@@ -2,16 +2,28 @@
 ### Reinstall btr_client from scratch.
 ### Useful for testing installation scripts.
 
+### get config settings from a file
+if [ "$1" = '' ]
+then
+    echo "Usage: $0 settings.sh"
+    exit 1
+fi
+settings=$1
+set -a
+source  $settings
+set +a
+
 export drupal_dir=/var/www/bcl
 export drush="drush --root=$drupal_dir"
 
+rm -rf $drupal_dir-bak
 mv $drupal_dir $drupal_dir-bak
 
-cd $(dirname $0)
-cd ../install/install-scripts/
-
+cd /var/www/github/btr_client/install/install-scripts/
 ./20-make-and-install.sh
 ./30-git-clone.sh
 ./40-configure.sh
 
-../config.sh
+### configure
+cd /var/www/github/btr_client/install/
+./config.sh
