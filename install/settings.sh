684dba2b7cbb19517be8ac8d573f9bfc69f4335c
Dashamir Hoxha <dashohoxha@gmail.com>
2014-06-19 14:27:03 +0000

    Fixed installation settings.

--- install/settings.sh
+++ install/settings.sh
@@ -2,13 +2,11 @@
 ### Default settings for building the chroot.
 target='bcl'
 arch='i386'
-#suite='precise'
 suite='trusty'
 apt_mirror='http://archive.ubuntu.com/ubuntu'
 
 ### Git branche that will be used.
-#bcl_git_branch='master'
-bcl_git_branch='trusty'
+bcl_git_branch='master'
 
 ### Domain of the website.
 bcl_domain='example.org'
