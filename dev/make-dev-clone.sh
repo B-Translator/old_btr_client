6ab4ac07c489ae8bbf50c396cc7a9d40717e5648
Dashamir Hoxha <dashohoxha@gmail.com>
2014-07-07 15:51:53 +0000

    Add a test user on bcl_dev.

--- dev/make-dev-clone.sh
+++ dev/make-dev-clone.sh
@@ -2,3 +2,6 @@
 
 ### make a clone of /var/www/bcl to /var/www/bcl_dev
 /var/www/code/btr_client/dev/clone.sh bcl bcl_dev
+
+### add a test user
+drush @bcl_dev user-create user1 --password=user1 --mail='user1@example.org'
