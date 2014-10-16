fc100e85abbed41b27c164f13c9d041f76b3c3a1
Dashamir Hoxha <dashohoxha@gmail.com>
2014-10-16 19:19:47 +0200

    Tighten remote login through ssh.

--- install/settings.sh
+++ install/settings.sh
@@ -29,3 +29,9 @@ oauth2_client_secret='0123456789'
 
 ### Install also extra things that are useful for development.
 development='true'
+
+### Login through ssh.
+### Only login through private keys is allowed.
+### See also this:
+###   http://dashohoxha.blogspot.com/2012/08/how-to-secure-ubuntu-server.html
+sshd_port=2201
